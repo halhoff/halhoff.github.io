aff781af769b712662d1fa8cc2d1e1a2c873dae7
halhoff <halhoffmeyer@gmail.com>
2024-09-05 10:46:44 -0400

    Update calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,3 +1,4 @@
-function add(a, b) {
-  
-}
+function calculate(input)
+
+const input = document.getElementById("input").value;
+document.getElementById("result").textContent = `Result: ${calculate(string)}`;
